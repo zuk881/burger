511f8ea9245ada557c2433e02bc0ac74b144ce8e
zuk881 <zuk881@cox.net>
2020-03-12 16:45:18 -0700

    new heroku attempt

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+ } else { 
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "lf7xkbag1nprxcx1",
-  password: "v02q99etxkx847l4",
+  user: "root",
+  password: "root",
   database: "burger_db"
-});
+ });
 
 // Make connection.
 connection.connect(function(err) {
