55acb2093293b1ae6a2814943df7081b03434896
zuk881 <zuk881@cox.net>
2020-03-12 15:25:54 -0700

    pushing to heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,8 @@
-INSERT INTO burger (name) VALUES ('Big Mac');
-INSERT INTO burger (name, eaten) VALUES ('Whopper', true);
+INSERT INTO burger (name) VALUES ('Dirt Worm Burger');
+INSERT INTO burger (name, eaten) VALUES ('Cowabunga Burger', true);
+INSERT INTO burger (name) VALUES ("Spicy Meatball Burger");
+INSERT INTO burger (name, eaten) VALUES ("Hot Sauce Burger", true);
+INSERT INTO burger (name) VALUES ("Michelangelo Burger");
+INSERT INTO burger (name) VALUES ("New York Special Burger");
+
+SELECT * FROM burger
